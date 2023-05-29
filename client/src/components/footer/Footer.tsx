bc65f0f202786b63887cac00419e1e62ca2b0f8a
Baum <luis.baumer1@gmail.com>
2023-05-29 21:50:57 +0200

    new

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,9 +1,53 @@
 import React from "react"
+import "./footer.css"
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="globalfooter rt-footer">
+      <section className="innerfooter">
+        <div className="__ftsc listsc">
+          <ul className="ftlink-list">
+            <h2 className="ftcaller">Overview</h2>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="/about">About</a>
+                </li>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="/story">How it wokrs</a>
+                </li>
+          </ul>
+        </div>
+        <div className="__ftsc listsc">
+          <ul className="ftlink-list">
+            <h2 className="ftcaller">Legal</h2>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="//grovider.co/privacy-policy">Privacy Policy</a>
+                </li>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="./imprint.html">Imprint</a>
+                </li>
+            </ul>
+        </div>
+        <div className="__ftsc listsc">
+          <ul className="ftlink-list">
+            <h2 className="ftcaller">Help</h2>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="/how-to-play.html">How to play</a>
+                </li>
+                <li className="ftlinkitem">
+                      <a className="ftlink" href="/contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div className="__ftsc copysc">
+          <div className="__logoftctcopy">
+            <img src="./img/rapidtyper-logo.png" alt="Logo" height={45} className="ftlogoimg" />
+          </div>
+          <div className="copytxtsect">
+            <p className="copyrighttxt">&copy;2023 RapidTyper</p>
+            <p className="copyby"><small>von Luis und Janick</small></p>
+          </div>
+        </div>
+      </section>
+    </footer>
   )
 }
