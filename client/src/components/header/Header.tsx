b430257042b757e5f45d0206d4846aa94f28ad96
Baum <luis.baumer1@gmail.com>
2023-05-29 21:23:17 +0200

    new-css

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
+import "./header.css"
 
 export default function Header({ children }: { children: React.ReactNode }) {
     const [loggedin] = useAuth()
     return (
         <div>
-            <nav>
+            <nav className="rt_navigation default_nav">
                 <div className="navigation-logo container-nav">
-                    <Link to="/">{/* index */}<img src="" alt="Logo" /></Link>
+                    <Link to="/" className="logoContainer">{/* index */}<img src="./img/rapidtyper-logo.png" alt="Logo" className="src_logo" /></Link>
                 </div>
                 <div className="navigation-link container-nav">
                     <ul className="_linklisting">
@@ -18,9 +19,22 @@ export default function Header({ children }: { children: React.ReactNode }) {
                             </Link>
                         </li>
                         <li className="nav-link-item">
-                            <Link to="/about">
-                                <span className="link_call">About</span>
+                            <Link to="#">
+                                <span className="link_call">Play</span>
+                                <span className="navicon"><svg className="d-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path></svg></span>
                             </Link>
+                            <ul className="dropdown_container" hidden>
+                                <li className="dropdown_item">
+                                    <Link to="/play/single">
+                                        <span className="link_call">Singleplayer</span>
+                                    </Link>
+                                </li>
+                                <li className="dropdown_item">
+                                    <Link to="/play/lobby">
+                                        <span className="link_call">Mulitplayer</span>
+                                    </Link>
+                                </li>
+                            </ul>
                         </li>
                         <li className="nav-link-item">
                             <Link to="/account">
--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     return (
         <div>
             <h2>Home</h2>
-            <button>
-                <Link className="test" to="/account">Account</Link>
-            </button>
+            <Link className="test" to="/account">
+                <button>Account</button>
+            </Link>
         </div>
     )
 }
