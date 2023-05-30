37df1b66030a0b9c831a90a0ec42514384f07820
Baum <luis.baumer1@gmail.com>
2023-05-30 16:18:35 +0200

    d

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react"
+import React, { useState, useRef } from "react"
 import { Link } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
 import "./header.css"
@@ -12,7 +12,7 @@ export default function Header({ children }: { children: React.ReactNode }) {
         setOpen(!state)
     }
     const collapseOutside = (e:any) =>{
-        if(open && !dropRef.current?.contains(e.target as Node))  setOpen(false)
+        // if(open && !dropRef.current?.contains(e.target as Node))  setOpen(false)
 
         // ↑↑ warum funktioniert das nicht?? ↑↑
     }
@@ -36,7 +36,7 @@ export default function Header({ children }: { children: React.ReactNode }) {
                                 <span className="link_call">Play</span>
                                 <span className="navicon"><svg className="d-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"></path></svg></span>
                             </Link>
-                            { open && (
+                            {open && (
                             <ul className="dropdown_container">
                                 <li className="dropdown_item" style={{marginBottom: 14}}>
                                     <Link to="/singleplayer" className="noscale">
