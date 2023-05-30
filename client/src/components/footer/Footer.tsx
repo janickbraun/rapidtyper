f50d9583823afbab50c8e56295e0be4a8f889369
Janick Braun <nasebuch@gmail.com>
2023-05-30 19:03:46 +0200

    insta

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -3,32 +3,35 @@ import "./footer.css"
 import { Link } from "react-router-dom"
 
 export default function Footer() {
-  return (
-    // footer
-<footer className="globalfooter rt-footer">
-  <section className="innerfooter">
-  <ul className="linksfooter">
-    <li className="footeritem">
-      <Link to="/about">About</Link>
-    </li>
-    <span>•</span>
-    <li className="footeritem">
-      <a href="//grovider.co/privacy-policy">Privacy Policy</a>
-    </li>
-    <span>•</span>
-    <li className="footeritem">
-      <a href="./imprint.html">Imprint</a>
-    </li>
-  </ul>
-  <div className="copyright">
-    <p className="copyrighttxt">&copy;2023 RapidTyper - von Luis und Janick</p>
-  </div>
-  </section>
-</footer>
-  )
+    return (
+        // footer
+        <footer className="globalfooter rt-footer">
+            <section className="innerfooter">
+                <ul className="linksfooter">
+                    <li className="footeritem">
+                        <Link to="/about">About</Link>
+                    </li>
+                    <span>•</span>
+                    <li className="footeritem">
+                        <a href="//grovider.co/privacy-policy">Privacy Policy</a>
+                    </li>
+                    <span>•</span>
+                    <li className="footeritem">
+                        <a href="./imprint.html">Imprint</a>
+                    </li>
+                </ul>
+                <div className="copyright">
+                    <p className="copyrighttxt">
+                        &copy;2023 RapidTyper - von Luis und{" "}
+                        <a style={{ textDecoration: "none", color: "white" }} target="_blank" rel="noreferrer" href="https://www.instagram.com/jan1ck.braun/">
+                            Janick
+                        </a>
+                    </p>
+                </div>
+            </section>
+        </footer>
+    )
 }
-
-
 
 /*
 
