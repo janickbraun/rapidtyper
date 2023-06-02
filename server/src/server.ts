cd3da5333ee7a6190ea85c04b2b1c213ae6a5c17
Janick Braun <nasebuch@gmail.com>
2023-06-02 22:24:07 +0200

    socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express"
 import dotenv from "dotenv"
 import helmet from "helmet"
+import { Server } from "socket.io"
 import cors from "cors"
 import bodyParser from "body-parser"
 import path from "path"
@@ -10,9 +11,23 @@ import mongoose from "mongoose"
 dotenv.config()
 
 const PORT = process.env.PORT
+const allowedOrigins = ["http://localhost", "http://localhost:5000", "https://grovider.co"]
 
 const app: Application = express()
 const server = http.createServer(app)
+const io: any = new Server(server, {
+    cors: {
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true)
+            if (allowedOrigins.indexOf(origin) === -1) {
+                let msg = "The CORS policy for this site does not " + "allow access from the specified Origin."
+                return callback(new Error(msg), false)
+            }
+            return callback(null, true)
+        },
+        methods: ["GET", "POST"],
+    },
+})
 
 mongoose.set("strictQuery", false)
 mongoose
@@ -28,11 +43,50 @@ mongoose.connection.on("error", (err) => {
     console.log(err)
 })
 
+let typists: any = []
+
+io.on("connection", (socket: any) => {
+    socket.on("join", (data: any) => {
+        socket.join(String(data.code))
+        if (data.code.length === 4) {
+            typists.push({
+                id: socket.id,
+                code: data.code,
+            })
+            try {
+                io.to(String(data.code)).emit("join", { msg: "new one", num: io.sockets.adapter.rooms.get(data.code).size })
+            } catch {}
+        }
+    })
+
+    socket.on("disconnect", () => {
+        console.log("disconnect")
+        if (typists.some((e: any) => e.id === socket.id)) {
+            for (let i = typists.length - 1; i >= 0; i--) {
+                if (typists[i].id === socket.id && typists[i].code.length === 4) {
+                    try {
+                        io.to(String(typists[i].code)).emit("leave", { msg: "someone left", num: io.sockets.adapter.rooms.get(typists[i].code).size })
+                    } catch {}
+
+                    typists.splice(i, 1)
+                    break
+                }
+            }
+        }
+    })
+
+    socket.on("leave", () => {
+        console.log("leave")
+    })
+})
+
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
+
+app.set("io", io)
 
 import indexRouter from "./routes/index"
 import signUpRouter from "./routes/account/signup"
