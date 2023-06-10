a7a35545d84ae007ae5b7ee5167534d9eb39a3be
Baum <luis.baumer1@gmail.com>
2023-06-10 22:37:13 +0200

    mein skin add

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -39,14 +39,12 @@ export default function Home() {
                 <>
                     <Link tabIndex={-1} to={"/user/" + username}>
                         <button>Profile</button>
-                    </Link>{" "}
+                    </Link>
                 </>
             )}
-            <br />
-            <br />
-            <Link tabIndex={-1} to="/singleplayer">
+            {/* <Link tabIndex={-1} to="/singleplayer">
                 <button>Singleplayer</button>
-            </Link>{" "}
+            </Link> */}
             {loggedin ? (
                 <>
                     <button onClick={handleMultiplayer}>Multiplayer</button>
@@ -70,7 +68,9 @@ export default function Home() {
                             <Link to={"/account#signup"}>
                                 <button className="primary_action largebtn">
                                     <span className="btninspan">Signup</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="clgoicon" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                                    <figure className="clcionct">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="clgoicon" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                                    </figure>
                                 </button>
                             </Link>
                         </div>
