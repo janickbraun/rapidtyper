75ab7f6d569129d4f2c6324b70913eabf80c101a
Baum <luis.baumer1@gmail.com>
2023-06-10 22:26:18 +0200

    login prompt

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
 import Leaderboard from "./Leaderboard"
+import "./home.main.css"
 
 export default function Home() {
     const [loggedin, username] = useAuth()
@@ -34,7 +35,6 @@ export default function Home() {
 
     return (
         <main>
-            <h1>Home</h1>
             {loggedin && (
                 <>
                     <Link tabIndex={-1} to={"/user/" + username}>
@@ -42,9 +42,6 @@ export default function Home() {
                     </Link>{" "}
                 </>
             )}
-            <Link tabIndex={-1} to="/account">
-                <button>Account</button>
-            </Link>
             <br />
             <br />
             <Link tabIndex={-1} to="/singleplayer">
@@ -56,7 +53,29 @@ export default function Home() {
                     {mutationMultiplayer.isError && <div>{mutationMultiplayer.error.message}</div>}
                 </>
             ) : (
-                <div>For playing online you need to login</div>
+                <div className="login_hint hct takeover_trans2"> 
+                    <div className="loginhint_inner">
+                        <div className="loginhintheader">
+                            <h1 className="trcaller">Unlock more</h1>
+                        </div>
+                        <div className="lowerhintbody cl">
+                            <p className="complete">Unlock skins, play multiplayer, and discover other amazing features exclusively for registered users.</p>
+                        </div>
+                        <div className="buttons_callercontainer">
+                            <Link to={"/account#login"}>
+                                <button className="secondary_action largebtn">
+                                    <span className="btninspan">Login</span>
+                                </button>
+                            </Link>
+                            <Link to={"/account#signup"}>
+                                <button className="primary_action largebtn">
+                                    <span className="btninspan">Signup</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="clgoicon" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             )}
             <Leaderboard />
         </main>
