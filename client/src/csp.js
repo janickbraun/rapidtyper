87f012cc282fb258a88233f2c813f27ecc0a3764
Baum <luis.baumer1@gmail.com>
2023-06-11 20:39:50 +0200

    ContentSecurityPolicy (!Janick!) bitte NPM install

---
+++ client/src/csp.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dev: {
+        "default-src": ["'self'"],
+        "style-src": ["*", "'insafe-inline'"]
+    },
+    prod: {
+        "default-src": "'self'",
+        "style-src": "'self'",
+        "img-src": ["'self'", "https://favicon.grovider.co"],
+        "object-src": ["'self'", "https://*.grovider.co"],
+        "connect-src": [ "'self'", "https://mybackend.com"]
+    }
+}
