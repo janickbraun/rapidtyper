c3741523b385abd458832c9bde8bf630d58ea3d2
Baum <luis.baumer1@gmail.com>
2023-06-12 21:03:57 +0200

    s

---
+++ client/src/components/footer/Terms.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./lg.css"
+
+export default function Terms() {
+  return (
+    <>
+            <div className="imprintReset" style={{marginTop: "7rem"}}>
+                <h1 id="en">Terms of Use</h1>
+                <p>Last update: Jun 12 2023</p>
+                <h2 id="m46"> __ </h2>
+                <p> _____  </p>
+                
+            </div>
+        </>
+  )
+}
