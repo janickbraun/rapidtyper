bf139ca2a4c5bf846fb97eeaaf7e87312d74e745
Baum <luis.baumer1@gmail.com>
2023-06-14 17:16:53 +0200

    css

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from "express"
 const router = Router()
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("Server is online")
+    res.send("API - 404")
 })
 
 export default router
