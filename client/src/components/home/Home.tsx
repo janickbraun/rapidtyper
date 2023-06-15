2cbcb5804eea5cef9a2081031e03fb76d5abb1ff
Janick Braun <nasebuch@gmail.com>
2023-06-15 22:09:43 +0200

    all special skins unlocked

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -6,6 +6,7 @@ import useAuth from "../../hooks/useAuth"
 import Leaderboard from "./Leaderboard"
 import "./home.main.css"
 import LearnMore from "../about/LearnMore"
+import { unlockSkin } from "../../helpers/skinHelper"
 
 function timeout(delay: number) {
     return new Promise((res) => setTimeout(res, delay))
@@ -57,9 +58,21 @@ export default function Home() {
         mutationMultiplayer.mutate()
     }
 
+    const handleSkin = (skin: string) => {
+        unlockSkin(skin)
+    }
+
     return (
         <main>
-            {accountResponse !== "" && <div className="infopopup">{accountResponse}<svg xmlns="http://www.w3.org/2000/svg" className="sndficon" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM296 336h-16V248C280 234.8 269.3 224 256 224H224C210.8 224 200 234.8 200 248S210.8 272 224 272h8v64h-16C202.8 336 192 346.8 192 360S202.8 384 216 384h80c13.25 0 24-10.75 24-24S309.3 336 296 336zM256 192c17.67 0 32-14.33 32-32c0-17.67-14.33-32-32-32S224 142.3 224 160C224 177.7 238.3 192 256 192z"/></svg></div>}
+            <button onClick={() => handleSkin("bee")}>Share</button>
+            {accountResponse !== "" && (
+                <div className="infopopup">
+                    {accountResponse}
+                    <svg xmlns="http://www.w3.org/2000/svg" className="sndficon" viewBox="0 0 512 512">
+                        <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM296 336h-16V248C280 234.8 269.3 224 256 224H224C210.8 224 200 234.8 200 248S210.8 272 224 272h8v64h-16C202.8 336 192 346.8 192 360S202.8 384 216 384h80c13.25 0 24-10.75 24-24S309.3 336 296 336zM256 192c17.67 0 32-14.33 32-32c0-17.67-14.33-32-32-32S224 142.3 224 160C224 177.7 238.3 192 256 192z" />
+                    </svg>
+                </div>
+            )}
             {loggedin && (
                 <>
                     <div className="account_supercontainer">
@@ -85,38 +98,38 @@ export default function Home() {
                                             </Link>
                                         </div>
                                     </div>
-                                <div className="profile_actionset">
-                                    <div className="__buttonset">
-                                        <>
-                                            {/* <button  className="largebtn">Play multiplayer</button> */}
-                                            <button onClick={handleMultiplayer} className="noscale">
-                                                <div className="nv-groupsvg">
-                                                    <svg className="cnvsvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                                                        <path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z" />
-                                                    </svg>
-                                                </div>
-                                                <div className="nvt">
-                                                    <h2 className="link_call large">Multiplayer</h2>
-                                                    <p className="description_sd2">Race against others</p>
-                                                </div>
-                                            </button>
-                                            {mutationMultiplayer.isError && <div>{mutationMultiplayer.error.message}</div>}
-                                        </>
-                                        <Link tabIndex={-1} to="/singleplayer">
-                                            <button className="noscale">
-                                                <div className="nv-groupsvg">
-                                                    <svg className="cnvsvg h37" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                                        <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                                    </svg>
-                                                </div>
-                                                <div className="nvt">
-                                                    <h2 className="link_call large">Singleplayer</h2>
-                                                    <p className="description_sd2">Practice your typing</p>
-                                                </div>
-                                            </button>
-                                        </Link>
+                                    <div className="profile_actionset">
+                                        <div className="__buttonset">
+                                            <>
+                                                {/* <button  className="largebtn">Play multiplayer</button> */}
+                                                <button onClick={handleMultiplayer} className="noscale">
+                                                    <div className="nv-groupsvg">
+                                                        <svg className="cnvsvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                                            <path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div className="nvt">
+                                                        <h2 className="link_call large">Multiplayer</h2>
+                                                        <p className="description_sd2">Race against others</p>
+                                                    </div>
+                                                </button>
+                                                {mutationMultiplayer.isError && <div>{mutationMultiplayer.error.message}</div>}
+                                            </>
+                                            <Link tabIndex={-1} to="/singleplayer">
+                                                <button className="noscale">
+                                                    <div className="nv-groupsvg">
+                                                        <svg className="cnvsvg h37" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div className="nvt">
+                                                        <h2 className="link_call large">Singleplayer</h2>
+                                                        <p className="description_sd2">Practice your typing</p>
+                                                    </div>
+                                                </button>
+                                            </Link>
+                                        </div>
                                     </div>
-                                </div>
                                 </div>
                             </div>
                         </div>
@@ -152,9 +165,7 @@ export default function Home() {
                             </Link>
                             <span>or</span>
                             <Link tabIndex={-1} to="/singleplayer">
-                                <button className="secondary_action">
-                                    Singleplayer
-                                </button>
+                                <button className="secondary_action">Singleplayer</button>
                             </Link>
                         </div>
                     </div>
