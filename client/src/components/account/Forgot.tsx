98bcd73b1fa378d082a6dfde8813f2fc0cb26f80
Janick Braun <nasebuch@gmail.com>
2023-06-16 19:36:56 +0200

    fixed imports

--- client/src/components/account/Forgot.tsx
+++ client/src/components/account/Forgot.tsx
@@ -1,11 +1,9 @@
 import { useMutation } from "@tanstack/react-query"
 import axios from "axios"
 import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 export default function Forgot() {
     const [email, setEmail] = useState("")
-    let navigate = useNavigate()
     const mutation: any = useMutation({
         mutationFn: async () => {
             return await axios.post(process.env.REACT_APP_BACKEND_URL + "/api/account/forgot", { email })
