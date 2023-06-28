2973ad06056f88fd6da72dfbe2968278c0587a1e
Baum <luis.baumer1@gmail.com>
2023-06-28 21:58:55 +0200

    shop new

--- client/src/components/shop/Shop.tsx
+++ client/src/components/shop/Shop.tsx
@@ -51,6 +51,10 @@ export default function Shop() {
         setConfirmOpen(false)
     }
 
+    const glowAssist:any = {
+        backgroundImage: `/img/skins/baum.png`
+    }
+
     return (
         <main>
             <h1>Shop</h1>
@@ -68,11 +72,22 @@ export default function Shop() {
             <br />
             {confirmOpen ? <Overlay /> : ""}
             {confirmOpen && (
-                <div>
-                    <img src={"/img/skins/" + skin + ".png"} alt={name} />
-                    Do your really want to buy "{name}" for {price} US $?
+                <div className="selector_container">
+                    <h1 className="modalCallerHeader">Confirm purchase</h1>
+                    <div className="close_container">
+                        <button className="close-modal-button" onClick={handleClose}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="csvg" viewBox="0 0 320 512">
+                                <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="contentdisplay">
+                        <div className="mask_contentparent" style={glowAssist}>
+                            <img src={"/img/skins/" + skin + ".png"} alt={name} className="shopimagedisplay" draggable="false"/>
+                        </div>
+                        Do your really want to buy "{name}" for {price} US $?
+                    </div>
                     <button onClick={handlePurchase}>Confirm</button>
-                    <button onClick={handleClose}>Cancel</button>
                 </div>
             )}
             {mutationBuy.isError && <div>{mutationBuy.error?.response?.data}</div>}
